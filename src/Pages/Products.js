0be5d3d4a72fc1faf03bf3bcf28099478a784033
Paya Shahmaleki <p.Shahmaleki@charisma.tech>
2024-02-20 16:37:09 +0330

    update

--- src/Pages/Products.js
+++ src/Pages/Products.js
@@ -5,16 +5,19 @@ import { DataGrid } from '@mui/x-data-grid';
 import DeleteOutlineIcon from '@mui/icons-material/DeleteOutline';
 import EditNoteOutlinedIcon from '@mui/icons-material/EditNoteOutlined';
 import { Link } from "react-router-dom";
+import { Dialog, DialogTitle, DialogActions, DialogContent, DialogContentText, Button } from "@mui/material";
 
 
 
 function Products() {
 
     const [ProductsList, setProductsList] = useState(AllProducts)
+    const [ShowDeleteModal, setShowDeleteModal] = useState(false)
 
     const DeleteProduct = (ProductID) => {
         setProductsList(ProductsList.filter((product) => product.id != ProductID))
     }
+
 
     const Columns = [
         { field: "id", headerName: "ID", width: 50 },
@@ -34,7 +37,7 @@ function Products() {
                         <Link to={`/Product/${params.row.id}`}>
                             <EditNoteOutlinedIcon className="products-Actions-edit"></EditNoteOutlinedIcon>
                         </Link>
-                        <DeleteOutlineIcon onClick={() => (DeleteProduct(params.row.id))} className="products-Actions-delete"></DeleteOutlineIcon>
+                        <DeleteOutlineIcon onClick={() => setShowDeleteModal(true)} className="products-Actions-delete"></DeleteOutlineIcon>
                     </div>
                 )
             }
@@ -59,6 +62,26 @@ function Products() {
                 checkboxSelection
                 disableRowSelectionOnClick
             />
+            <Dialog
+                open={ShowDeleteModal}
+                aria-labelledby="alert-dialog-title"
+                aria-describedby="alert-dialog-description"
+            >
+                <DialogTitle id="alert-dialog-title" style={{ color: "red" }}>
+                    Delete Item !
+                </DialogTitle>
+                <DialogContent>
+                    <DialogContentText id="alert-dialog-description">
+                        Are You Sure To Delete ?
+                    </DialogContentText>
+                </DialogContent>
+                <DialogActions>
+                    <Button onClick={() => setShowDeleteModal(false)} >Disagree</Button>
+                    <Button onClick={(params) => DeleteProduct(params.row.id)} autoFocus>
+                        Agree
+                    </Button>
+                </DialogActions>
+            </Dialog>
         </div>
     )
 }
